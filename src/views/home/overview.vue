2c64d1475381751f794db5a1a64d0f97d85b72c1
Royin <raoyingjun@foxmail.com>
2022-06-23 14:27:22 +0800

    切换发/收文tab交互效果补充

--- src/views/home/overview.vue
+++ src/views/home/overview.vue
@@ -4,10 +4,13 @@
     <el-main>
       <base-card>
         <template #label>
-          <el-button @click="$router.push({ name: routeName.HomeOverViewSendDocumentList })">发文列表</el-button>
-          <el-button class="ml-4" @click="$router.push({ name: routeName.HomeOverViewIncomingDocumentList })"
-            >收文列表
-          </el-button>
+          <el-button
+            v-for="tab in tabs"
+            :key="tab.routeName"
+            :class="[tab.className, activeTab === tab.routeName ? 'active' : '']"
+            @click="$router.push({ name: tab.routeName })"
+            >{{ tab.tabName }}</el-button
+          >
         </template>
         <router-view />
       </base-card>
@@ -22,4 +25,36 @@
 import DataBoard from './components/data-board.vue';
 import { routeName } from '../../router/enum';
 import Notice from './components/notice.vue';
+import { useRoute } from 'vue-router';
+import { ref, computed } from 'vue';
+
+const route = useRoute();
+const tabs = ref([
+  {
+    tabName: '发文列表',
+    routeName: routeName.HomeOverViewSendDocumentList,
+  },
+  {
+    tabName: '收文列表',
+    routeName: routeName.HomeOverViewIncomingDocumentList,
+    className: 'ml-4',
+  },
+]);
+
+const activeTab = computed(() => route.name || routeName.HomeOverViewSendDocumentList);
+
+// const pushTo =
 </script>
+<style scoped lang="scss">
+// 统一 button 样式
+:deep(.el-button) {
+  padding: 9px 18px;
+  font-weight: bold;
+  background: rgba(0, 0, 0, 0.02);
+  border: 1px solid #ebebeb;
+  &.active {
+    color: #1573ff;
+    background: white;
+  }
+}
+</style>
