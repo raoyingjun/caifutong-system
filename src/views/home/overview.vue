6ffdcba5956301e69540c4aa0097c705450ff7a4
Royin <raoyingjun@foxmail.com>
2022-06-24 10:59:22 +0800

    修复复用子路由页面样式不对的问题

--- src/views/home/overview.vue
+++ src/views/home/overview.vue
@@ -12,7 +12,9 @@
             >{{ tab.tabName }}</el-button
           >
         </template>
-        <router-view />
+        <div class="sub-module-common-style">
+          <router-view />
+        </div>
       </base-card>
     </el-main>
     <el-aside width="456px">
@@ -54,6 +56,42 @@ const activeTab = computed(() => route.name || routeName.HomeOverViewSendDocumen
   &.active {
     color: #1573ff;
     background: white;
+  }
+}
+// 子路由页面 发文列表&收文列表 通用样式
+:deep(.sub-module-common-style) {
+  .el-form-item {
+    .el-form-item__content {
+      width: 216.67px;
+    }
+
+    .el-form-item__label,
+    .el-input,
+    .el-date-editor {
+      height: 28px;
+      line-height: 28px;
+    }
+
+    margin: {
+      right: 0;
+      bottom: 16px;
+    }
+  }
+  .el-divider {
+    &.el-divider--horizontal {
+      margin: {
+        top: 0;
+        bottom: 16px;
+      }
+    }
+  }
+  .el-button {
+    padding: 6px 16px;
+  }
+  .el-table {
+    .el-table__cell {
+      padding: 8px 0;
+    }
   }
 }
 </style>
