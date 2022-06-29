a9f5acc071b8f4708bf1526398d8b7531897ad62
Royin <raoyingjun@foxmail.com>
2022-06-29 09:48:14 +0800

    完成外部收文查看详情静态页面

--- src/views/external-incoming-document/components/internal-receiver-info.vue
+++ src/views/external-incoming-document/components/internal-receiver-info.vue
@@ -1,6 +1,8 @@
 <template>
   <base-card label="内部接收人信息">
-    <div class="justify-end"><el-button link type="primary" style="margin-right: 6%">批量催办</el-button></div>
+    <div class="justify-end">
+      <el-button link type="primary" style="margin-right: 6%">批量催办</el-button>
+    </div>
     <el-descriptions :column="1">
       <el-descriptions-item label="传阅人（主送）">
         <span v-for="(people, index) in circulatedMainSendPeople" :key="index">
@@ -14,14 +16,16 @@
           <el-link type="primary" class="mh-16" :underline="false">催办</el-link>
         </span>
       </el-descriptions-item>
-      <el-descriptions-item label="收文摘要" style="line-height: 19.6px"
-        >这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要</el-descriptions-item
-      >
+      <el-descriptions-item label="收文摘要" style="line-height: 19.6px">
+        这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要这里是收文摘要
+      </el-descriptions-item>
     </el-descriptions>
+    <need-reply-people />
   </base-card>
 </template>
 <script setup>
 import { ref } from 'vue';
+import NeedReplyPeople from './need-reply-people.vue';
 
 const circulatedMainSendPeople = ref(
   Array(6)
---
+++ src/views/external-incoming-document/components/need-reply-people.vue
@@ -0,0 +1,61 @@
+<template>
+  <base-table :total="120" class="mt-24" :data="needReplyPeople">
+    <el-table-column label="需回复人" prop="people"></el-table-column>
+    <el-table-column label="需回复事项" prop="event" show-overflow-tooltip></el-table-column>
+    <el-table-column
+      label="回复内容"
+      prop="content"
+      :formatter="({ content }) => content || '-'"
+      show-overflow-tooltip
+    ></el-table-column>
+    <el-table-column label="操作">
+      <template #default="{ row }">
+        <template v-if="row.content">
+          <template v-if="row.attachment">
+            <el-icon style="vertical-align: middle"><Link /></el-icon>
+            <el-link :underline="false" type="primary" class="ml-16">预览</el-link>
+            <el-link :underline="false" type="primary" class="ml-8">下载</el-link>
+          </template>
+          <template v-else>-</template>
+        </template>
+        <el-link v-else :underline="false" type="primary">催办</el-link>
+      </template>
+    </el-table-column>
+  </base-table>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const needReplyPeople = ref([
+  {
+    people: 'vivianxu(许微微)',
+    event: '这里是需回复事项这里是需回复事项这里是需回复事项',
+    content: '这里是回复内容这里是回复内容这里是回复内容这里是回复内容',
+  },
+  {
+    people: 'vivianxu(许微微)',
+    event: '这里是需回复事项这里是需回复事项这里是需回复事项',
+    content: '',
+  },
+  {
+    people: 'vivianxu(许微微)',
+    event: '这里是需回复事项这里是需回复事项这里是需回复事项',
+    content: '这里是回复内容这里是回复内容这里是回复内容这里是回复内容',
+    attachment: '这里是附件',
+  },
+]);
+</script>
+<style scoped lang="scss">
+:deep(.el-table) {
+  $marginLeft: 56px;
+  width: calc(100% - $marginLeft);
+  margin-left: $marginLeft;
+  & .el-table__cell {
+    padding: 10px 0;
+    &.is-leaf {
+      padding: 15px 0;
+    }
+  }
+}
+</style>
