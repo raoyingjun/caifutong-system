e6ff5d7d4019689cb19c0b8919c8fc7a73933750
Royin <raoyingjun@foxmail.com>
2022-06-30 10:32:31 +0800

    优化文件上传样式

--- src/components/base-upload.vue
+++ src/components/base-upload.vue
@@ -1,13 +1,25 @@
 <template>
-  <el-upload class="upload-demo" drag action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15" multiple>
-    <el-icon class="el-icon--upload"><upload-filled /></el-icon>
-    <div class="el-upload__text">Drop file here or <em>click to upload</em></div>
-    <template #tip>
-      <div class="el-upload__tip">jpg/png files with a size less than 500kb</div>
-    </template>
+  <el-upload drag action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15" multiple>
+    <el-icon class="mt-36" color="#1573ff" :size="36">
+      <upload-filled />
+    </el-icon>
+
+    <div class="el-upload__text">将文件拖入此处，或<em>点击上传</em></div>
   </el-upload>
 </template>
-
-<script setup lang="ts">
-import { UploadFilled } from '@element-plus/icons-vue';
-</script>
+<style scoped lang="scss">
+:deep(.el-upload) {
+  width: 634px;
+  height: 131.69px;
+}
+:deep(.el-upload-dragger) {
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  background: #fbfdff;
+  border: 1px solid rgba(21, 115, 255, 0.56);
+}
+.el-upload__text {
+  line-height: 14px;
+}
+</style>
