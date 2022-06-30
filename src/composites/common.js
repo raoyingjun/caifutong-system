32ac15e8cc2487cf56f55ab847d6a49652ec2956
Royin <raoyingjun@foxmail.com>
2022-06-30 16:27:18 +0800

    新增分页组合式api

--- src/composites/common.js
+++ src/composites/common.js
@@ -1,10 +1,6 @@
 import { reactive, toRefs } from 'vue';
 
-export const usePagination = () => {
-  const pagination = reactive({
-    currentPage: 1,
-    pageSize: 10,
-    total: 0,
-  });
+export const usePagination = (currentPage = 1, pageSize = 10, total = 0) => {
+  const pagination = reactive({ currentPage, pageSize, total });
   return toRefs(pagination);
 };
