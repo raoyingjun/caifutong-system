9ffbcba4e56a363b3744ee23c617db9ed495db35
Royin <raoyingjun@foxmail.com>
2022-06-30 10:05:06 +0800

    抽离样式冗杂代码；优化全局样式

--- src/views/external-incoming-document/new-incoming-document.vue
+++ src/views/external-incoming-document/new-incoming-document.vue
@@ -2,8 +2,8 @@
   <incoming-document-creation-info-form />
   <send-document-info-form />
   <div class="justify-end g-operation g-fixed g-fixed-bottom">
-    <el-button class="cancel">取消</el-button>
-    <el-button class="preview">预览</el-button>
+    <el-button type="info" plain>取消</el-button>
+    <el-button plain type="primary">预览</el-button>
     <el-button class="save-draft">保存草稿</el-button>
     <el-button type="primary">提交</el-button>
   </div>
@@ -20,20 +20,11 @@ import SendDocumentInfoForm from './components/send-document-info-form.vue';
     height: 45px;
     margin-left: 32px;
   }
-  .cancel {
-    color: #999;
-    background: white;
-    border: 1px solid #999;
-    opacity: 1;
-  }
-  .preview {
-    color: #005ad8;
-    background: white;
-    border: 1px solid #005ad8;
-  }
   .save-draft {
     color: white;
     background: rgba(39, 189, 255, 1);
   }
 }
 </style>
+
+<style></style>
--- src/views/external-incoming-document/view-detail.vue
+++ src/views/external-incoming-document/view-detail.vue
@@ -2,9 +2,22 @@
   <incoming-document-creation-info />
   <send-document-info />
   <internal-receiver-info />
+  <div class="justify-end g-operation g-fixed g-fixed-bottom">
+    <el-button type="info" plain>撤回</el-button>
+    <el-button type="primary" plain>返回</el-button>
+  </div>
 </template>
 <script setup>
 import IncomingDocumentCreationInfo from './components/incoming-document-creation-info.vue';
 import SendDocumentInfo from './components/send-document-info.vue';
 import InternalReceiverInfo from './components/internal-receiver-info.vue';
 </script>
+<style scoped lang="scss">
+.g-operation {
+  :deep(.el-button) {
+    width: 128px;
+    height: 40px;
+    margin-left: 32px;
+  }
+}
+</style>
