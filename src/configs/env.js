4fc962709535bad2b14fecdf2aa73f75775c849e
Royin <raoyingjun@foxmail.com>
2022-07-04 16:16:09 +0800

    优化环境文件

--- src/configs/env.js
+++ src/configs/env.js
@@ -1,7 +1,8 @@
 export const getMode = () => import.meta.env.MODE;
 export const isDev = () => import.meta.env.DEV;
 export const isProd = () => import.meta.env.PROD;
-export const getApiHost = (devApiPrefixPath = '/api') => (isDev() ? devApiPrefixPath : import.meta.env.VITE_API_HOST);
+export const getApiHost = (devApiPrefixPath = '/api') =>
+  isDev() ? devApiPrefixPath : import.meta.env.VITE_API_HOST || '';
 export const getApiVersion = () => import.meta.env.VITE_API_VERSION;
 export const getApiBaseUrl = (devApiPrefixPath = '/api') => `${getApiHost(devApiPrefixPath)}/${getApiVersion()}`;
 const UPLOAD_FILE_URL = 'upload-files';
