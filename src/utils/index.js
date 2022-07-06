5a89aec8e88dfdd4a386d74e6a661482aa46c052
Royin <raoyingjun@foxmail.com>
2022-07-06 10:05:19 +0800

    补充工具函数注释

--- src/utils/index.js
+++ src/utils/index.js
@@ -30,8 +30,11 @@ export default {
 };
 
 /**
- * @example findLabelByValue(1, [{label: 'A', value: 0},{label: 'B', value: 1}], ['label', 'value'])
- * @param value
+ * @example findLabelByValue(1, [{label: 'A', value: 0},{label: 'B', value: 1}], ['label', 'value']) => 'A'
+ * @param {Number | String} value 要查找的 value
+ * @param {Object} options 选项列表。形如 [{label: 'A', value: 0},{label: 'B', value: 1}]
+ * @param {[String, String]} kvMap 对应选项列表的 label 和 value 字段名。形如：['label', 'value']
+ * @return 查找到的 value 对应 value
  */
 export const findLabelByValue = (value, options, kvMap = ['label', 'value'] /* [label key, value key] */) =>
   options.find((option) => option[kvMap[1]] === value)?.[kvMap[0]];
