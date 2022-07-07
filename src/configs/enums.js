0e11e27d435bfb6c72049737dac754880db12202
Royin <raoyingjun@foxmail.com>
2022-07-07 09:23:32 +0800

    补充枚举映射

---
+++ src/configs/enums.js
@@ -0,0 +1,14 @@
+// 审批状态
+export const ApprovalStatusMap = {
+  Validating: '校对中',
+  Signing: '会签中',
+  IssueRejected: '签发驳回',
+  SignRejected: '会签驳回',
+  UsingSeal: '用印中',
+  SealCompleted: '用印完成',
+  SealFailed: '用印失败',
+};
+
+// export const ApprovalStatusTextMap = {
+//   Validating: '校对中'
+// }
