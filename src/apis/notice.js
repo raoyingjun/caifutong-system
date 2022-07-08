d403b4b50e038dc000317dd61a8cf2ec2ec3022f
Royin <raoyingjun@foxmail.com>
2022-07-08 11:16:05 +0800

    补充公告接口

--- src/apis/notice.js
+++ src/apis/notice.js
@@ -2,8 +2,16 @@ import http from '@/utils/http';
 
 // 以下是接口定义示例
 export default {
-  // 获取外部收文列表
+  // 获取公告
   getNoticeList: (postData) => {
     return http.post('/notice/list', postData);
+  },
+  // 设置公告
+  updateNotice: (postData) => {
+    return http.put('/notice', postData);
+  },
+  // 删除公告
+  deleteNotice: (ids) => {
+    return http.post('/notice/delete', ids);
   },
 };
